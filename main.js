6642d5152558e0e61ffcf4cdac4cfbc5d030c759
Aryaveer-Coding <79976916+Aryaveer-Coding@users.noreply.github.com>
2021-12-08 16:40:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,47 @@
+noseX = 0;
+noseY = 0;
+
 function preload()
 {
-
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup()
 {
-    canvas = createCanvas(640, 480);
-    canvas.position(110, 250);
+    canvas = createCanvas(500, 400);
+    canvas.center();
     video = createCapture(VIDEO);
+    video.size(500, 400);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 40;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
 }
 
 function draw()
 {
-    image(video, 0, 0, 640, 480);
-
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-    circle(30, 30, 40);
-
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-    circle(610, 30, 40);
-
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-    circle(30, 450, 40);
-
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-    circle(610, 450, 40);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    rect(50, 20, 540, 20);
-    rect(50, 440, 540, 20);
-    rect(20, 50, 20, 380);
-    rect(600, 50, 20, 380);
-
-    
-   
-}    
+    image(video, 0, 0, 500, 400);
+    image(clown_nose, noseX, noseY, 80, 35);
+}
 
 function take_snapshot()
 {
-    save('student_name.png');
+    save('myFilterImage.png');
 }
